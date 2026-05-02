5f68cc78b19b264cdf75e37abcbb47699e7deded
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-05-02 14:18:10 -0300

    feat: resolve exercícios de operadores aritméticos da Seção 1

---
+++ python/Matematica_Aplicada/Lista_P1/01_operadores.py
@@ -0,0 +1,18 @@
+# Seção 1: Operadores Aritméticos
+
+# 1. Complete para obter 100 
+print(10 ** 2) # Potenciação: 10 elevado a 2 [cite: 7, 8]
+
+# 2. Complete para obter 3 
+print(10 // 3) # Divisão inteira: quantas vezes o 3 cabe no 10 [cite: 11, 12]
+
+# 3. Complete para obter 1
+print(10 % 3)  # Resto da divisão: o que sobra de 10 dividido por 3 [cite: 14, 15]
+
+# 4. Qual será a saída? 
+# (10 // 5) resulta em 2. Então, 2 * 2 = 4.
+print((10 // 5) * 2) # Saída: 4 [cite: 18, 19]
+
+# 5. Qual será a saída? 
+# 2**3 = 8 | 3 / 2 = 1.5 | 8 + 1.5 = 9.5
+print(2**3 + 3 / 2) # Saída: 9.5 [cite: 22]
