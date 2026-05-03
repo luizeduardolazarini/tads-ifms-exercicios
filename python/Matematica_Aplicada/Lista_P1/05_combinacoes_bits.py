df652155d63821ae4e1710de0164b04cf027ff19
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-05-03 16:38:32 -0300

    cálculo de combinações de bits

---
+++ python/Matematica_Aplicada/Lista_P1/05_combinacoes_bits.py
@@ -0,0 +1,7 @@
+# Exercício 11b - Matemática Aplicada (IFMS)
+# Objetivo: Calcular o número de combinações possíveis com 8 bits (2^8).
+
+bits = 8
+combinacoes = 2 ** bits
+
+print(combinacoes) # Saída esperada: 256
