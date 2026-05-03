6c0e2cef9c66e5604d7060b9c72599fc8124f0c0
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-05-03 17:06:40 -0300

    feat: lógica de aprovação simples com if/else

---
+++ python/Matematica_Aplicada/Lista_P1/16_verificacao_aprovacao_nota.py
@@ -0,0 +1,12 @@
+nota = 7.5
+
+# Análise para a prova:
+# A condição é (nota >= 7). 
+# Como 7.5 é maior que 7, o código executa o bloco do 'if'.
+
+if nota >= 7:
+    print('Aprovado')
+else:
+    print('Reprovado')
+
+# Saída esperada: Aprovado
