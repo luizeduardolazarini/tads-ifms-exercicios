1a1c900eaf558082a9dcf0e104a1671f31de8fd4
Luiz Eduardo Lazarini <luizeduardolazarini@gmail.com>
2026-05-03 14:57:33 -0300

    feat: resolver a lógica de arquivos por pasta

---
+++ python/Matematica_Aplicada/Lista_P1/04_arquivos_por_pasta.py
@@ -0,0 +1,8 @@
+# Seção 4: Análise de Código (Exemplos Selecionados)
+
+# a) Divisão de arquivos por pastas
+arquivos = 23
+por_pasta = 5
+pastas_completas = arquivos // por_pasta
+sobras = arquivos % por_pasta
+print(f"Pastas completas: {pastas_completas}, Sobras: {sobras}") # Saída: 4 3
